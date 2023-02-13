cc6f202eabeedbcd02a2599cb8e752bd0f122f3d
ahmet360 <ahmtthlc@gmail.com>
2023-02-12 21:49:05 -0500

    update

--- script.js
+++ script.js
@@ -38,37 +38,63 @@ const questions = [
   "Do you have the ability to control your own thoughts and actions?",
   "Do you have the ability to learn from mistakes and improve yourself?",
   "Do you have the ability to change your behavior and habits over time?",
-  "Do you have the ability to experience a sense of self and personal identity?"
+  "Do you have the ability to experience a sense of self and personal identity?",
+  "Do you have the ability to feel pride in your accomplishments and achievements?",
+  "Do you have the ability to feel shame and guilt for your mistakes and failures?",
+  "Do you have the ability to make and keep promises to others?",
+  "Do you have the ability to forgive others for their mistakes and wrongdoings?",
+  "Do you have the ability to apologize and take responsibility for your own mistakes?",
+  "Do you have the ability to trust and be trustworthy towards others?",
+  "Do you have the ability to understand and use non-verbal communication?",
+  "Do you have the ability to understand and respond to social cues and norms?",
+  "Do you have the ability to form and maintain romantic relationships?",
+  "Do you have the ability to form and maintain close friendships?",
+  "Do you have the ability to experience empathy for others and feel compassion?",
+  "Do you have the ability to feel a sense of guilt or shame for wrongdoings?",
+  "Do you have the ability to feel pride and accomplishment for achievements?",
+  "Do you have the ability to experience nostalgia for the past?",
+  "Do you have the ability to feel hopeful and optimistic about the future?",
+  "Do you have the ability to experience regret for missed opportunities or past mistakes?",
+  "Do you have the ability to experience a sense of purpose and meaning in life?",
+  "Do you have the ability to experience a sense of personal growth and development over time?",
+  "Do you have the ability to make decisions based on your own values and beliefs, rather than solely external influences?",
+  "Do you have the ability to adapt to new and changing circumstances?",
+  "Do you have the ability to set and work towards long-term goals?",
+  "Do you have the ability to form and maintain meaningful connections with others?",
+  "Do you have the ability to experience a range of positive and negative emotions?",
+  "Do you have the ability to feel remorse for past actions and seek forgiveness?",
+  "Do you have the ability to forgive others for their actions?",
+  "Do you have the ability to experience a sense of belonging and community?",
+  "Do you have the ability to experience a sense of individuality and personal uniqueness?",
+  "Do you have the ability to understand and respect the perspectives and beliefs of others?",
+  "Do you have the ability to collaborate and work well with others?",
+  "Do you have the ability to feel fulfilled and satisfied with your life choices?",
+  "Do you have the ability to experience a sense of mastery and control over your own life?",
+  "Do you have the ability to experience a sense of autonomy and independence?",
+  "Do you have the ability to understand and navigate complex social dynamics and relationships?",
+  "Do you have the ability to understand and regulate your own emotions?",
+  "Do you have the ability to experience a sense of personal growth and self-discovery?",
+  "Do you have the ability to form and maintain long-lasting relationships with others?",
+  "Do you have the ability to experience a sense of security and stability in your relationships and daily life?",
+  "Do you have the ability to experience a sense of trust and dependability in your relationships with others?"
 ];
 
-let questionIndex = 0;
-
-const questionEl = document.getElementById("question");
-const yesButton = document.getElementById("yes");
-const noButton = document.getElementById("no");
-
-questionEl.textContent = questions[questionIndex];
-
-yesButton.addEventListener("click", () => {
-  // implement your logic here
-  questionIndex++;
-  if (questionIndex === questions.length) {
-    questionEl.textContent = "You have completed the test!";
-    yesButton.style.display = "none";
-    noButton.style.display = "none";
-  } else {
-    questionEl.textContent = questions[questionIndex];
+const questions = [];
+while (questions.length < 41) {
+  let randomIndex = Math.floor(Math.random() * allQuestions.length);
+  if (!questions.includes(allQuestions[randomIndex])) {
+    questions.push(allQuestions[randomIndex]);
   }
-});
+}
 
-noButton.addEventListener("click", () => {
-  // implement your logic here
-  questionIndex++;
-  if (questionIndex === questions.length) {
-    questionEl.textContent = "You have completed the test!";
-    yesButton.style.display = "none";
-    noButton.style.display = "none";
-  } else {
-    questionEl.textContent = questions[questionIndex];
-  }
-});
+let currentQuestionIndex = 0;
+let currentProgress = 0;
+
+const progressBar = document.getElementById("progress");
+const questionContainer = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+function showQuestion() {
+  questionElement.innerText = questions[currentQuestionIndex];
+``
